f508accfcf4b97995fd1cf9b34ce3ec83a54de82
Amalchristo123 <139036853+Amalchristo123@users.noreply.github.com>
2023-10-29 19:40:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from os import environ as env
 import asyncio, datetime, time
 
 
-ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅"
-START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use"
+ACCEPTED_TEXT = "Hey {user}\n\nYour Request To Join {chat} Has Approved !! ✅"
+START_TEXT = "Hai {}\n\nAdd This Bot To Your Channel To Accept Join Requests Automatically 😊"
 
 API_ID = int(env.get('API_ID'))
 API_HASH = env.get('API_HASH')
